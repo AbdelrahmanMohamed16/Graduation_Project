546823ed1c9f1931c1bbdc7e9e2b88b61a18e840
AbdelrahmanMoahmed16 <11410120210229@stud.cu.edu.eg>
2024-12-02 04:53:14 +0200

    fixing import bugs v0.3

--- project/src/pages/NounsPage/NounsPage.jsx
+++ project/src/pages/NounsPage/NounsPage.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import Section1 from "../../sections/Section1";
 import DividerComponent from "../../components/Divider/DividerComponent";
 import Section3 from "../../sections/Section3";
-import LastSection from "../../sections/LastSection/LastSection";
 import BigSection from "../../sections/BigSection/BigSection";
 import TabSection from "../../sections/TabSection/TabSection";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,6 +10,7 @@ import LoadingPage from "../LoadingPage/LoadingPage";
 import Swal from "sweetalert2";
 import { clearSavedState } from "../../redux/userSlice";
 import Section2 from "../../sections/Section2/Section2";
+import LastSection from "../../sections/LastSection/LastSection";
 
 export default function NounsPage(props) {
   const [value, setValue] = useState(-1);
